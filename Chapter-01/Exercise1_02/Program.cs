5ae70a9704908bb730cdcea28d1f326056d61e45
owen_0918 <owen.martin@savethechildren.org>
2023-06-18 10:24:20 +0600

    Exeercise 03

---
+++ Chapter-01/Exercise1_02/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hi! I'm your first program. What is your name?");
+var name = Console.ReadLine();
+Console.WriteLine($"Hi {name}, it is very nice to meet you. We have a really fun journey ahead.");
---
+++ Chapter-01/Exercise1_03/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+static void FormatString(string stringToFormat) {
+    stringToFormat.Replace("World", "Mars");
+}
+
+static string FormatReturningString(string stringToFormat) {
+    return stringToFormat.Replace("Earth", "Mars");
+}
+
+var greetings = "Hello World!";
+FormatString(greetings);
+Console.WriteLine(greetings);
+var anotherGreetings = "Good morning Earth!";
+Console.WriteLine(FormatReturningString(anotherGreetings));
