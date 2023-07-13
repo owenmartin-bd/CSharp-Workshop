5cb9914a55d6c239b5bab8ceea6d47d9111b87f0
owen_0918 <owen.martin@savethechildren.org>
2023-07-13 09:18:21 +0600

    Exercise1_05

---
+++ Chapter-01/Exercise1_04/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Type a value for a: ");
+var a = int.Parse(Console.ReadLine());
+Console.WriteLine("Now type a value for b: ");
+var b = int.Parse(Console.ReadLine());
+Console.WriteLine($"The value for a is {a} and for b is {b}");
+Console.WriteLine($"Sum: {a+b}");
+Console.WriteLine($"Multiplication: {a*b}");
+Console.WriteLine($"Substraction: {a-b}");
+Console.WriteLine($"Division: {a/b}");
